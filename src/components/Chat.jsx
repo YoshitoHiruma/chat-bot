2ce2106796babf523186b024aaf03325107cdbf5
yoshitooo <saikoo.orange@gmail.com>
2020-07-19 16:22:18 +0900

    Initial Commit

---
+++ src/components/Chat.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Guest from "../assets/img/guest.jpg";
+import Host from "../assets/img/host.jpg";
+
+const Chat = (props) => {
+  const isQuestion = props.type === "question";
+  const classes = isQuestion ? "p-chat__row" : "p-chat__reverse";
+  return (
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={Host} />
+        ) : (
+          <Avatar alt="icon" src={Guest} />
+        )}
+      </ListItemAvatar>
+      <div className="p-chat__bubble">{props.text}</div>
+    </ListItem>
+  );
+};
+
+export default Chat;
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqDvDRDr-vNqrlPOD9VT_d4ne8yNbADo0",
+    authDomain: "chat-bot-4328b.firebaseapp.com",
+    databaseURL: "https://chat-bot-4328b.firebaseio.com",
+    projectId: "chat-bot-4328b",
+    storageBucket: "chat-bot-4328b.appspot.com",
+    messagingSenderId: "947992062856",
+    appId: "1:947992062856:web:6874bc9459d77d335fe83d",
+    measurementId: "G-6RDN9VTZKP"
+};
+
+export default firebaseConfig;
